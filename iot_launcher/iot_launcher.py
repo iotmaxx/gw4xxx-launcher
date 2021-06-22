d33750fbc089609de750a73f95ab01f76b368218
Ralf Glaser <glaser@iotmaxx.de>
2021-06-22 17:28:19 +0200

    parse configuration and start selected application

--- iot_launcher/iot_launcher.py
+++ iot_launcher/iot_launcher.py
@@ -2,13 +2,42 @@ import subprocess
 import json
 from pathlib import Path
 
+jsonConfigDir = "/config/iot_launcher.d"
+jsonConfigFile = "/config/iot_launcher.json"
+
 anyviz = {
-    "application": ["/usr/bin/anyvizcloudadapter"],
-    "description": "Anyviz Cloud Adapter",
-    "type": "cloudadapter",
-    "publisher": {
-        "name": "Mirasoft GmbH",
-        "URL": "www.anyviz.de"
+    "anyviz": {
+        "application": ["/usr/bin/anyvizcloudadapter"],
+        "description": "Anyviz Cloud Adapter",
+        "type": "cloudadapter",
+        "publisher": {
+            "name": "Mirasoft GmbH",
+            "URL": "www.anyviz.de"
+        }
+    }
+}
+
+test1 = {
+    "test1": {
+        "application": ["/usr/bin/anyvizcloudadapter"],
+        "description": "Anyviz Cloud Adapter",
+        "type": "cloudadapter",
+        "publisher": {
+            "name": "Mirasoft GmbH",
+            "URL": "www.anyviz.de"
+        }
+    }
+}
+
+test2 = {
+    "test2": {
+        "application": ["/usr/bin/anyvizcloudadapter"],
+        "description": "Anyviz Cloud Adapter",
+        "type": "cloudadapter",
+        "publisher": {
+            "name": "Mirasoft GmbH",
+            "URL": "www.anyviz.de"
+        }
     }
 }
 
@@ -18,12 +47,31 @@ launcher_default = {
 
 #set up default (start anyviz) in case this is an update from an older version without iot_launcher
 try:
-    Path("/config/iot_launcher.d").mkdir()
+    Path(jsonConfigDir).mkdir()
     with open("/config/iot_launcher.d/anyviz.json", "x") as jfile:
         json.dump(anyviz, jfile)
-    with open("/config/iot_launcher.json", "x") as jfile:
+    with open("/config/iot_launcher.d/test1.json", "x") as jfile:
+        json.dump(test1, jfile)
+    with open("/config/iot_launcher.d/test2.json", "x") as jfile:
+        json.dump(test2, jfile)
+    with open(jsonConfigFile, "x") as jfile:
         json.dump(launcher_default, jfile)
 except FileExistsError:
     pass
 
+configFiles = Path(jsonConfigDir).glob("*.json")
+
+theServices = {}
+
+for file in configFiles:
+    with open(file, "r") as jfile:
+        theServices.update(json.load(jfile))
+
+with open(jsonConfigFile, "r") as jfile:
+    launcherConfig = json.load(jfile)
+
+subprocess.run(theServices[launcherConfig["launch"]]["application"])
+
+#for service in theServices:
+ #   print(service)
 
