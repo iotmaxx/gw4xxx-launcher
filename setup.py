5fd9c2c78c4fc544c13c169310a226b0fa05da97
Ralf Glaser <glaser@iotmaxx.de>
2021-06-23 13:46:13 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gw4xxx-launcher',
+    version='0.1dev',
+    packages=['iot_launcher',],
+    description='Gw-CLI to change network settings on Linux based systems',
+    author='Ralf Glaser',
+    author_email='glaser@iotmaxx.de',
+    url='https://github.com/iotmaxx/gw4xxx-launcher',
+ #   py_modules=['gw_cli'],
+)
